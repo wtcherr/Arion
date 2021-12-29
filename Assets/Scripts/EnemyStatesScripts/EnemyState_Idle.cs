682550e19e4eeb3ebae4f5cb5489c8e11916ea47
wtcherr <53377426+loda99@users.noreply.github.com>
2021-12-29 17:34:57 +0200

    Adfd

--- Assets/Scripts/EnemyStatesScripts/EnemyState_Idle.cs
+++ Assets/Scripts/EnemyStatesScripts/EnemyState_Idle.cs
@@ -5,7 +5,16 @@ public class EnemyState_Idle : EnemyStateData
 {
 	public override void checkState(EnemyStateManager stateManager, Animator animator)
 	{
-		active=true;
+		List<EnemyStateData> currentStates=stateManager.getCurrentStates();
+		bool ok=true;
+		foreach(EnemyStateData state in currentStates){
+			if(state.stateName!=EnemyStatesNames.idle){
+				ok=false;
+			}
+		}
+		if(ok)
+			active=true;
+		else active=false;
 	}
 
 	public override bool checkTrigger(EnemyStateManager stateManager, Animator animator)
