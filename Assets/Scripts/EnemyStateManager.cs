1e213c5f6b08fb42e868d4f32e672bfa65060122
wtcherr <53377426+loda99@users.noreply.github.com>
2021-12-29 18:50:23 +0200

    Fixed Errors

--- Assets/Scripts/EnemyStateManager.cs
+++ Assets/Scripts/EnemyStateManager.cs
@@ -90,4 +90,7 @@ public class EnemyStateManager : MonoBehaviour
             this.transform.localScale=scale;
         }
     }
+    public List<EnemyStateData>getCurrentStates(){
+        return currentStates;
+    }
 }
